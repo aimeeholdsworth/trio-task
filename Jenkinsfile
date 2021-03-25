661df1d7af90b092ff7cc13670f10d860bd7d038
Ubuntu <ubuntu@ip-172-31-26-224.eu-west-2.compute.internal>
2021-03-25 11:13:48 +0000

    setting jenkins docker login

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,9 +2,15 @@ pipeline{
     agent any
     envrionmet{
         MYSQL_ROOT_PASSWORD = credentials("MYSQL_ROOT_PASSWORD")
+        DOCKER_PASSWORD = credentials("DOCKER_PASSWORD")
 
     }
     stages{
+        stage("Install Dependencies"){
+            steps {
+                sh "bash install-dependencies.sh"
+
+            }
         stage("Build"){
             steps {
                 sh "docker-compose build --parrallel"
---
+++ install-dependencies.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+sudo apt-get update
+
+#docker install
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker jenkins
+
+#docker compose install
+# make sure jq & curl is installed
+sudo apt update
+sudo apt install -y curl jq
+# set which version to download (latest)
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# download to /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# make the file executable
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+#authenitcate docker user with jenkins
+echo $DOCKER_PASSWORD | docker login --username aimeeh97 --pasword-stdin 
